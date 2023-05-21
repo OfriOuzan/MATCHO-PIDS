da20d70dbc828ad0f1d4b8acde1ef16a2431de54
OfriOuzan <104366208+OfriOuzan@users.noreply.github.com>
2023-05-21 14:14:24 +0300

    Update matcho-pids.py

--- matcho-pids.py
+++ matcho-pids.py
@@ -85,7 +85,7 @@ def get_running_pids():
 
 def arguments():
     """This function sets the arguments."""
-    parser = argparse.ArgumentParser(description="'MATCHO-PIDS is an automation tool that match between containers and host PIDs.")
+    parser = argparse.ArgumentParser(description="MATCHO-PIDS is an automation tool that match between containers and host PIDs.")
     parser.add_argument('-p', '--pids', type=str, nargs='+', default=False, help='Get a list host processes PIDs to be '
                                                                                  'matched with container PIDs')
     parser.add_argument('-f', '--format', type=str, default='text', help='Specify output formatter: csv or text, default'
